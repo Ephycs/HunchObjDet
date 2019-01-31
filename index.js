46ca4f9a4b9f7beae31679fe2a9abeec11b1d4b7
Ethan Feldman <unethanfeldman@gmail.com>
2019-01-31 18:45:07 -0500

    changed where files are located 2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ http.createServer(function(request, response) {
       return;
     }
 
-    if (fs.statSync(filename).isDirectory()) filename += 'sites/index.html';
+    if (fs.statSync(filename).isDirectory()) filename += 'WebpagesHub/index.html';
 
     fs.readFile(filename, "binary", function(err, file) {
       if(err) {        
